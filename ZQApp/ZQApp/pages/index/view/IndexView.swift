5dfbdb0b68ef76047bd300a2e86043ba849695c3
weizhiqiang <weizhiqiang@itcast.cn>
2021-03-05 19:27:38 +0800

    删除无用代码

--- ZQApp/ZQApp/pages/index/view/IndexView.swift
+++ ZQApp/ZQApp/pages/index/view/IndexView.swift
@@ -9,33 +9,6 @@ import SwiftUI
 import Moya
 
 struct IndexView: View {
-//    @State private var indexVM:IndexViewModel = IndexViewModel()
-    
-//    @State private var res : Bool = false
-//
-//    func contentLoaded() -> Bool {
-//        //Ask viewModel if content is loaded
-//        indexVM.request{ (_ bSuccess:Bool, _ data: [IndexModel]?) in
-//            res = bSuccess
-//            if(bSuccess) {
-//                //成功
-////                loadedBody
-//            } else {
-//                //失败
-////                unloadedBody
-//            }
-//        }
-//        return res
-//    }
-//
-//    var loadedBody : some View {
-//        Text("content is loaded")
-//    }
-//
-//    var unloadedBody: some View {
-//        Text("Fetching content...")
-//    }
-    
     @ObservedObject var indexVM : IndexViewModel = IndexViewModel()
     var body: some View {
         AsyncContentView(source: indexVM) { data in
--- ZQApp/ZQApp/pages/index/viewModel/IndexViewModel.swift
+++ ZQApp/ZQApp/pages/index/viewModel/IndexViewModel.swift
@@ -13,39 +13,8 @@ class IndexViewModel : LoadableObject {
     var indexData : [IndexModel]? = nil
     let provider = MoyaProvider<Service>()
     
-    //    @Published private(set) var state = LoadingState<[IndexModel]?>.idle
     @Published private(set) var state = LoadingState<[IndexModel]?>.idle
     
-    var data:[IndexModel]? { get {
-        return self.indexData
-    }  set {
-        self.indexData = newValue
-    }
-    }
-    //    var state:LoadingState {
-    //        return state
-    //    }
-    
-    //    init() {
-    //        self.state = LoadingState<Output>.idle
-    //    }
-    //    @Published private(set) var state = State.idle
-    
-    //    override init() {
-    //    }
-    //    func request(completion: @escaping (_ bSuccess:Bool, _ result:[IndexModel]?) -> Void ){
-    //        ApiLoadingProvider.request(Service.index, model:[IndexModel].self ) { [self] (bSuccess, returnData) in
-    //            if(bSuccess) {
-    //                //成功
-    //                self.indexData = returnData
-    //            } else {
-    //                //失败
-    //                self.indexData = nil
-    //            }
-    //            completion(bSuccess, self.indexData)
-    //        }
-    //    }
-    //
     func load() {
         state = .loading
         
@@ -57,7 +26,7 @@ class IndexViewModel : LoadableObject {
             } else {
                 //失败
                 self?.indexData = nil
-//                self?.state = .failed(Error(""))
+                //self?.state = .failed(Error(""))
             }
         }
     }
